6db066a751e819b74f3d9b5a6fe6fecc43673a02
minji9988 <rnwjf111@daum.net>
2022-12-11 14:32:23 +0900

    test1

---
+++ app/src/main/java/com/chobo/main_ver_2/TodayAllergy.java
@@ -0,0 +1,25 @@
+package com.chobo.main_ver_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public
+class TodayAllergy extends AppCompatActivity {
+
+//    ImageView todayallegyfournicon;
+
+    @Override
+    protected
+    void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today_allergy);
+
+//        todayallegyfournicon = (ImageView)findViewById(R.id.today_Allegy_FournIcon);
+//
+//        todayallegyfournicon.bringToFront();
+
+    }
+}
